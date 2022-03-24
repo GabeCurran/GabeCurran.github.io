60340b2579b12c433c7d6c076d4b2d8c2b6a927d
GabeCurran <gabecurran01@gmail.com>
2022-03-23 22:13:01 -0400

    Remove replace paragraphs function

--- docs/main.js
+++ docs/main.js
@@ -32,11 +32,6 @@ const homeParagraphs = [
     "My current goal is to graduate from ; ; and look for a job or internship in ; ; ; ; while I'm doing it. In the distant future, I hope to pursue a career in ; ; as well."
 ];
 
-const paragraphHTML = [
-    "I'm a <span class='highlighted'>student</span> with a passion for <span class='highlighted'>computers</span>, <span class='highlighted'>web design</span>, and <span class='highlighted'>video games</span>. I'm always looking for ways to <span class='highlighted'>learn more</span> and <span class='highlighted'>better myself</span>.",
-    "My current goal is to graduate from <span class='highlighted'>Thaddeus Stevens</span> and look for a job or internship in <span class='highlighted'>software or game development</span> while I'm doing it. In the distant future, I hope to pursue a career in <span class='highlighted'>data science</span> as well."
-]
-
 let typedParagraphs = document.querySelectorAll('.typedParagraph');
 
 function sleep(ms) {
@@ -58,13 +53,6 @@ async function type(typedParagraphs, homeParagraphs) {
         counter++;
         await sleep(250);
     }
-    // replaceParagraphs(typedParagraphs, homeParagraphs);
 }
-
-function replaceParagraphs() {
-    for (let paragraph = 0; paragraph < paragraphHTML.length; paragraph++) {
-        typedParagraphs[paragraph].innerHTML = paragraphHTML[paragraph];
-    }
-};
 
 type(typedParagraphs, homeParagraphs);
