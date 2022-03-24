919890437de4db0f9e47e6e45ed53b9812fd64ea
GabeCurran <gabecurran01@gmail.com>
2022-03-24 09:11:29 -0400

    The Smoothening

--- docs/main.js
+++ docs/main.js
@@ -1,4 +1,5 @@
 let wordCounter = 0;
+let pCounter = 0;
 
 const highlightedWords = [
     "student",
@@ -21,22 +22,32 @@ const highlightedWords = [
     "science"
 ];
 
-function span() {
-    let word = highlightedWords[wordCounter];
-    wordCounter++;
-    return "<span class='highlighted'>" + word + "</span>";
-}
+function span(letter) {
+    return "<span class='highlighted'>" + letter + "</span>";
+};
 
 const homeParagraphs = [
     "I'm a ; with a passion for ;, ; ;, and ; ;. I'm always looking for ways to ; ; and ; ;.",
     "My current goal is to graduate from ; ; and look for a job or internship in ; ; ; ; while I'm doing it. In the distant future, I hope to pursue a career in ; ; as well."
 ];
 
+const builtParagraphs = [
+    "I'm a <span class='highlighted'>student</span> with a passion for <span class='highlighted'>computers</span>, <span class='highlighted'>web design</span>, and <span class='highlighted'>video games</span>. I'm always looking for ways to <span class='highlighted'>learn more</span> and <span class='highlighted'>better myself</span>.",
+    "My current goal is to graduate from <span class='highlighted'>Thaddeus Stevens</span> and look for a job or internship in <span class='highlighted'>software or game development</span> while I'm doing it. In the distant future, I hope to pursue a career in <span class='highlighted'>data science</span> as well."
+];
+
 let typedParagraphs = document.querySelectorAll('.typedParagraph');
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-}
+};
+
+function replaceParagraphs() {
+    for (paragraph of typedParagraphs) {
+        paragraph.innerHTML = builtParagraphs[pCounter];
+        pCounter++;
+    }
+};
 
 async function type(typedParagraphs, homeParagraphs) {
     let counter = 0;
@@ -44,16 +55,20 @@ async function type(typedParagraphs, homeParagraphs) {
     for (paragraph of homeParagraphs) {
         for (letter of paragraph) {
             if (letter == ";") {
-                await sleep(13)
-                typedParagraphs[counter].innerHTML += span(typedParagraphs[counter]);
+                for (l of highlightedWords[wordCounter]) {
+                    typedParagraphs[counter].innerHTML += span(l);
+                    await sleep(25);
+                }
+                wordCounter++;
             } else {
                 typedParagraphs[counter].innerHTML += letter;
+                await sleep(25);
             }
-            await sleep(17);
         }
         counter++;
         await sleep(250);
     }
-}
+    replaceParagraphs();
+};
 
 type(typedParagraphs, homeParagraphs);
