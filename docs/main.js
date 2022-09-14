8cb7cfaa35806390c1969dc8161cc9810ad69441
GabeCurran <gabecurran01@gmail.com>
2022-09-14 15:38:39 -0400

    update info

--- docs/main.js
+++ docs/main.js
@@ -17,6 +17,7 @@ let originalParagraphs = document.querySelectorAll(".originalParagraph");
 const highlightedWords = [
     "student",
     "computers",
+    "software",
     "web",
     "design",
     "video",
@@ -27,12 +28,12 @@ const highlightedWords = [
     "myself",
     "Drexel",
     "University",
-    "software",
-    " / ",
-    "game",
-    "development",
+    "Ory",
     "data",
-    "science"
+    "science",
+    "/",
+    "game",
+    "development"
 ];
 
 function span(letter) {
@@ -40,13 +41,13 @@ function span(letter) {
 };
 
 const homeParagraphs = [
-    "I'm a ; with a passion for ;, ; ;, and ; ;. I'm always looking for ways to ; ; and ; ;.",
-    "My current goal is to graduate from ; ; and look for a job or internship in ; ; ; ; while I'm doing it. In the distant future, I hope to pursue a career in ; ; as well."
+    "I'm a ; with a passion for ;, ;, ; ;, and ; ;. I'm always looking for ways to ; ; and ; ;.",
+    "My current goal is to graduate from ; ; and continue my work at ; while I'm doing it. In the distant future, I hope to pursue a career in ; ; ; ; ; as well."
 ];
 
 const builtParagraphs = [
-    "I'm a <span class='highlighted'>student</span> with a passion for <span class='highlighted'>computers</span>, <span class='highlighted'>web design</span>, and <span class='highlighted'>video games</span>. I'm always looking for ways to <span class='highlighted'>learn more</span> and <span class='highlighted'>better myself</span>.",
-    "My current goal is to graduate from <span class='highlighted'><a href='https://drexel.com/' class='college' target='_blank'>Drexel University</a></span> and look for a job or internship in <span class='highlighted'>software / game development</span> while I'm doing it. In the distant future, I hope to pursue a career in <span class='highlighted'>data science</span> as well."
+    "I'm a <span class='highlighted'>student</span> with a passion for <span class='highlighted'>computers</span>, <span class='highlighted'>software</span>, <span class='highlighted'>web design</span>, and <span class='highlighted'>video games</span>. I'm always looking for ways to <span class='highlighted'>learn more</span> and <span class='highlighted'>better myself</span>.",
+    "My current goal is to graduate from <span class='highlighted'><a href='https://drexel.com/' class='college' target='_blank'>Drexel University</a></span> and continue my work at <span class='highlighted'><a href='https://ory.sh/' class='ory' target='_blank'>Ory</a></span> while I'm doing it. In the distant future, I hope to pursue a career in <span class='highlighted'>data science / game development</span> as well."
 ];
 
 let typedParagraphs = document.querySelectorAll('.typedParagraph');
@@ -84,6 +85,11 @@ async function type(typedParagraphs, homeParagraphs) {
                 if (highlightedWords[wordCounter] == 'University') {
                     let p = document.querySelector('#secondP');
                     p.innerHTML = "My current goal is to graduate from <span class='highlighted'><a href='https://drexel.com/' class='college' target='_blank'>Drexel University</a></span>";
+                }
+                // Add link to Ory
+                if (highlightedWords[wordCounter] == 'Ory') {
+                    let p = document.querySelector('#secondP');
+                    p.innerHTML = "My current goal is to graduate from <span class='highlighted'><a href='https://drexel.com/' class='college' target='_blank'>Drexel University</a></span> and continue my work at <span class='highlighted'><a href='https://ory.sh/' class='ory' target='_blank'>Ory</a></span>";
                 }
                 wordCounter++;
             } else {
