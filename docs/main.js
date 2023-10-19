33a43d33f4ebd0ef36f61473e9ace6fed91ea041
GabeCurran <gabecurran01@gmail.com>
2023-10-19 10:15:26 -0400

    update info

--- docs/main.js
+++ docs/main.js
@@ -58,12 +58,12 @@ function span(letter) {
 
 const homeParagraphs = [
     "I'm a ; with a passion for ;, ;, ; ;, and ; ;. I'm always looking for ways to ; ; and ; ;.",
-    "My current goal is to graduate from ; ; and continue working while I'm doing it. In the distant future, I hope to pursue a career in ; ; ; ; ; as well."
+    "My current goal is to graduate from ; ; and continue working while I'm doing it. I plan to pursue a career in ; ; ; ; ; as well."
 ];
 
 const builtParagraphs = [
     "I'm a <span class='highlighted'>student</span> with a passion for <span class='highlighted'>computers</span>, <span class='highlighted'>software</span>, <span class='highlighted'>web design</span>, and <span class='highlighted'>video games</span>. I'm always looking for ways to <span class='highlighted'>learn more</span> and <span class='highlighted'>better myself</span>.",
-    "My current goal is to graduate from <span class='highlighted'><a href='https://drexel.com/' class='college drexel' target='_blank'>Drexel University</a></span> and continue working while I'm doing it. In the distant future, I hope to pursue a career in <span class='highlighted'>data science / game development</span> as well."
+    "My current goal is to graduate from <span class='highlighted'><a href='https://drexel.com/' class='college drexel' target='_blank'>Drexel University</a></span> and continue working while I'm doing it. I plan to pursue a career in <span class='highlighted'>data science / game development</span> as well."
 ];
 
 let typedParagraphs = document.querySelectorAll('.typedParagraph');
